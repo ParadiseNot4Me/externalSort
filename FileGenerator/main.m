ba69d50b190aeaeab2ab5cae55209ddfcfa5ec09
Ivan Grigoriev <ig@poloniumarts.com>
2016-12-25 22:04:28 +0300

    [add] file generator target

---
+++ FileGenerator/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  FileGenerator
+//
+//  Created by Иван Григорьев on 25.12.16.
+//  Copyright © 2016 IG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
