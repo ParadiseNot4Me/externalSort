da25460b154e54d9a6f11f918e03c8f8e251c518
Ivan Grigoriev <ig@poloniumarts.com>
2016-12-25 22:03:41 +0300

    init commit

---
+++ ExternalSort/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  ExternalSort
+//
+//  Created by Иван Григорьев on 25.12.16.
+//  Copyright © 2016 IG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    
+    return 0;
+}
