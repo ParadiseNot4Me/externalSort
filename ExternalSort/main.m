51641b665b6483c4cfaee490c3419832c9408b4e
Ivan Grigoriev <ig@poloniumarts.com>
2016-12-26 00:02:03 +0300

    [add] print log, timeSpent

--- ExternalSort/main.m
+++ ExternalSort/main.m
@@ -11,8 +11,20 @@
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        clock_t begin, end;
+        begin = clock();
+        
         IGExternalSort *externalSort = [IGExternalSort new];
         [externalSort sortInputFilePath:argv[2] outputFilePath:argv[3] availableMemory:atoi(argv[4])*1000*1000];
+        
+        end = clock();
+        double timeSpent = (double) (end - begin) / CLOCKS_PER_SEC;
+        
+        printf("input file: %s\n", argv[2]);
+        printf("output file: %s\n", argv[3]);
+        printf("available memory: %d\n", argv[4]);
+        printf("%lf\n", timeSpent);
+        
     }
     return 0;
 }
