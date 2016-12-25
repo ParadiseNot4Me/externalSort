86dcbcf572b41da63caf4a20895f282e8085fddb
Ivan Grigoriev <ig@poloniumarts.com>
2016-12-25 22:06:10 +0300

    [add] file generator model

---
+++ FileGenerator/FileGenerator/IGFileGenerator.h
@@ -0,0 +1,13 @@
+//
+//  IGFileGenerator.h
+//  ExternalSort
+//
+//  Created by Иван Григорьев on 25.12.16.
+//  Copyright © 2016 IG. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface IGFileGenerator : NSObject
+
+@end
---
+++ FileGenerator/FileGenerator/IGFileGenerator.m
@@ -0,0 +1,13 @@
+//
+//  IGFileGenerator.m
+//  ExternalSort
+//
+//  Created by Иван Григорьев on 25.12.16.
+//  Copyright © 2016 IG. All rights reserved.
+//
+
+#import "IGFileGenerator.h"
+
+@implementation IGFileGenerator
+
+@end
